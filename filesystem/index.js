20befe5c4a12801d3d0010455e5762203e507ce8
AnnisaDwiF <annisadwif92@gmail.com>
2023-03-16 19:48:52 +0700

    add filesystem

---
+++ filesystem/index.js
@@ -0,0 +1,10 @@
+const fs = require(`fs`);
+
+const fileReadCallback = (error, data) => {
+    if (error) {
+        console.log(`Gagal membaca berkas`);
+        return;
+    }
+    console.log(data);
+};
+fs.readFile(`./filesystem/notes.txt`, `utf-8`, fileReadCallback);
